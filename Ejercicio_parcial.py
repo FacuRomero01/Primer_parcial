685d0138465e888297e3906104a55e02b886eed3
Facundo Romero <142251154+FacuRomero01@users.noreply.github.com>
2025-05-23 15:32:38 -0300

    Add files via upload

--- Ejercicio_parcial.py
+++ Ejercicio_parcial.py
@@ -12,21 +12,24 @@ def calcular_promedio(lista:list,valor:int)->bool:
     También retorna False si la lista está vacía.
     """
     
+# Parámetros formales: lista, valor
     suma_lista = 0
     for i in range(len(lista)):
         suma_lista += lista[i]
-    
-    promedio_lista = suma_lista/len(lista)
+
+    promedio_lista = suma_lista / len(lista)
     if promedio_lista > valor:
         res = True
     else:
         res = False
     return res
 
-entrada = [10,20,30,40]
+# Parámetros actuales: entrada = [10, 20, 30, 40], valor = 40
+entrada = [10, 20, 30, 40]
 valor = 40
 
-if calcular_promedio(entrada,valor):
+# Invocación de la función
+if calcular_promedio(entrada, valor):
     print("\nEl promedio de la lista dada es mayor al valor dado\n")
 else:
     print("\nEl promedio de la lista dada es menor al valor dado\n")
